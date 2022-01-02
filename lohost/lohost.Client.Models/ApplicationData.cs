f3507d835c6f025942abfc8e4208895f80329ea2
Gareth <gareth.marland@gmail.com>
2022-01-02 23:49:12 +0000

    working on default address

--- lohost/lohost.Client.Models/ApplicationData.cs
+++ lohost/lohost.Client.Models/ApplicationData.cs
@@ -4,7 +4,7 @@
     {   
         public ApplicationData()
         {
-            ApplicationId = System.Guid.NewGuid().ToString();
+            ApplicationId = GenerateDefaultAddress();
 
             ApplicationFolder = "App";
 
@@ -84,6 +84,22 @@
             if (!Directory.Exists(logsFolder)) Directory.CreateDirectory(logsFolder);
 
             return logsFolder;
+        }
+
+        private string GenerateDefaultAddress()
+        {
+            char[] chars = new char[] { 
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            Random r = new Random();
+
+            string address = string.Empty;
+
+            for (int i = 0; i<5; i++) address += chars[r.Next(0, chars.Length)];
+
+            return address;
         }
     }
 }
